d0d36a349804f415b220f606d24b20c8b4cf8ed0
zzzhim <3218566389@qq.com>
2021-12-19 16:11:32 +0800

    Dart常用数据类型（字符串）

--- lib/data_type.dart
+++ lib/data_type.dart
@@ -10,6 +10,7 @@ class _State extends State<DataType> {
   @override
   Widget build(BuildContext context) {
     _numType();
+    _stringType();
     return Container(
       child: Text('常用数据类型，请查看控制台输出'),
     );
@@ -28,5 +29,22 @@ class _State extends State<DataType> {
     print(num1.abs()); // 求绝对值
     print(num1.toInt()); // 转换成int
     print(num2.toDouble()); // 转换成double
+  }
+
+  // 字符串
+  void _stringType() {
+    String str1 = '字符串1', str2 = "字符串2"; // 字符串定义
+    String str3 = str1 + str2;
+    String str4 = "字符串拼接\$: $str1 and $str2";
+    String str5 = "常用数据类型，请查看控制台输出";
+
+    print(str1);
+    print(str2);
+    print(str3);
+    print(str4);
+
+    // 常用方法
+    print(str5.substring(0, 5));
+    print(str5.indexOf('控制'));
   }
 }
